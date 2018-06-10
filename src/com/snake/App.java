8cb2f3cda9929562ed4d4ffeeec658120cde59a3
Dawid <prawica.dawid@gmail.com>
2018-06-10 13:59:16 +0200

    ok

---
+++ src/com/snake/App.java
@@ -0,0 +1,15 @@
+package com.snake;
+
+import com.snake.logic.Run;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new Run();
+    }
+}
---
+++ src/com/snake/logic/Run.java
@@ -0,0 +1,8 @@
+package com.snake.logic;
+
+public class Run {
+
+    public Run(){
+        System.out.println("Hello snake");
+    }
+}

87c29ecef9e608dcdd95464b457ffea4b69c1b2a
Dawid <prawica.dawid@gmail.com>
2018-06-13 21:39:50 +0200

    engine

---
+++ src/com/snake/engine/Engine.java
@@ -0,0 +1,14 @@
+package com.snake.engine;
+
+public class Engine {
+
+    int preferSize = 20;
+
+    public Engine(){}
+
+    public Engine(int preferSize){
+        this.preferSize = preferSize;
+    }
+
+
+}
--- src/com/snake/logic/Run.java
+++ src/com/snake/logic/Run.java
@@ -1,9 +1,12 @@
 package com.snake.logic;
 
+import com.snake.engine.Engine;
 import com.snake.gui.GameGui;
 
 public class Run {
     public Run(String[] args){
+        Engine engine = new Engine(20);
+
         GameGui gameGui = new GameGui();
         gameGui.run(args);
     }

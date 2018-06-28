48fbd3b51b2aa151d37b059129a37cf46dd77b02
Dawid <prawica.dawid@gmail.com>
2018-06-28 21:32:04 +0200

    extract exclude

--- src/com/snake/engine/SnakeFood.java
+++ src/com/snake/engine/SnakeFood.java
@@ -1,5 +1,21 @@
 package com.snake.engine;
 
+import com.snake.dao.Snake;
+
+import java.util.List;
+
 public class SnakeFood {
-    
+
+    private List<Snake> snakeTail;
+    public SnakeFood (){}
+
+    private int[] extractExclude(){
+        return null;
+    }
+
+    private void setUpSnakeTail(List<Snake> snakeTail){
+
+    }
+
+
 }

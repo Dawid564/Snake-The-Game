47260bda4ab91dc38b279cb87352c1b7c9b8a513
Dawid <prawica.dawid@gmail.com>
2018-06-29 04:29:44 +0200

    implement generator logic

--- src/com/snake/engine/SnakeFood.java
+++ src/com/snake/engine/SnakeFood.java
@@ -8,25 +8,31 @@ import java.util.Random;
 public class SnakeFood {
 
     private Random rand;
-    private List<Snake> snakeTail;
 
     public int[] initFood(int[] x, int[] y, List<Snake> snakeTail){
+        int lenOfMap = convertToNum(x,y);
+        int[] excludes = calculateExclude(x,y,snakeTail);
         return null;
     }
 
-    private void init(){
-
-    }
-
-    private int[] extractExclude(){
+    private int[] calculateExclude(int[] x, int[] y, List<Snake> snakeTail){
+        int[] excludes = new int[snakeTail.size()-1];
+        for(int i=0; i<snakeTail.size()-1; i++){
+            excludes[i] = calcExcludePos(findIndex(x,snakeTail.get(i)), findIndex(y,snakeTail.get(i)));
+        }
         return null;
     }
 
-
-
-    private void setUpSnakeTail(List<Snake> snakeTail){
-
+    private int calcExcludePos(int a, int b){
+        return 0;
     }
 
+    private int findIndex(int[] array, Snake snake){
+        return 0;
+    }
+
+    private int convertToNum(int[] x, int[] y){
+        return x.length * y.length;
+    }
 
 }
